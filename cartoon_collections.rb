a380c7f328b1a26a151992b0e0115da3fec1ba0a
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 03:49:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(calls_long)
     end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
