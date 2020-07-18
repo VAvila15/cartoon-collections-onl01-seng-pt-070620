3614d4783afcb15627776fa47a997788477c7d39
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 03:22:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do { |call| planeteer_calls.capitalize }
+  planeteer_calls.map do |call|
     call + ("!" and planeteer_calls.capitalize)
   end
 end
