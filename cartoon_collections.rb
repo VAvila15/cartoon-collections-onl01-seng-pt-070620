62050f6d75edc64f1fbe7d0369c34ed3def6393f
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 02:16:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(veggies)
-  veggies.
+  new_array = []
+  veggies.join("!")
+  new_array << 
 end
 
 def long_planeteer_calls# code an argument here
