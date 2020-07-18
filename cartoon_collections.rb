f0b744dbc26efb6623a9ebad7bf09edf49e977dc
Victor Avila <avilavictor1570@gmail.com>
2020-07-18 02:20:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 def summon_captain_planet(veggies)
   new_array = []
   veggies.join("!")
-  new_array << "veggies".upcase
+  new_array.length << "veggies".upcase
 end
 
 def long_planeteer_calls# code an argument here
